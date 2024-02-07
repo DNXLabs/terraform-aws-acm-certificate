6c27613934044ab24994ebcfad1791e16fad355c
Figo Huang <figo.huang@creditorwatch.com.au>
2024-02-07 13:16:16 +1100

    fixing-resourceID-reach-length-32-limit

--- route53.tf
+++ route53.tf
@@ -1,6 +1,9 @@
+resource "random_uuid" "records" {
+}
+
 resource "aws_route53_record" "validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : random_uuid.records.result => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
